e97ba0ec593d361969cbc0530441508e25542749
rpgloria13 <diegogloria14@gmail.com>
2017-03-28 10:54:36 +0800

    Add files via upload
    added Trivia view

---
+++ views/profile/_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Profile */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="profile-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'cname')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'nname')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'email')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'home_address')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'gender')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'cpnumber')->textInput() ?>
+
+    <?= $form->field($model, 'comments')->textarea(['rows' => 6]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ views/trivia/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Trivia */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="trivia-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id')->textInput() ?>
+
+    <?= $form->field($model, 'question')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'answer')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'profile_id')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
